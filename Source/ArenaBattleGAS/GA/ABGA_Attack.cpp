3ad7e8a0afc0bde2cb49f84544a41a1baebced71
Ghostory <ume6514@gmail.com>
2025-08-30 14:09:01 +0900

    4

--- Source/ArenaBattleGAS/GA/ABGA_Attack.cpp
+++ Source/ArenaBattleGAS/GA/ABGA_Attack.cpp
@@ -8,6 +8,8 @@
 #include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
 #include "ArenaBattleGAS.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Character/ABComboActionData.h"
+
 UABGA_Attack::UABGA_Attack()
 {
 	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
@@ -19,9 +21,10 @@ void UABGA_Attack::ActivateAbility(const FGameplayAbilitySpecHandle Handle, cons
 	
 	// 어택 모션 몽타주 재생
 	AABCharacterBase* ABCharacter = CastChecked<AABCharacterBase>(ActorInfo->AvatarActor.Get());
+	CurrentComboData = ABCharacter->GetComboActionData();
 	ABCharacter->GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_None);
 
-	UAbilityTask_PlayMontageAndWait* PlayAttackTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, TEXT("PlayAttack"),ABCharacter->GetComboActionMontage());
+	UAbilityTask_PlayMontageAndWait* PlayAttackTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, TEXT("PlayAttack"),ABCharacter->GetComboActionMontage(),1.0f,GetNextSection());
 	
 	//종료 시 발생하는 델리게이트
 	PlayAttackTask->OnCompleted.AddDynamic(this, &UABGA_Attack::OnCompleteCallback);
@@ -29,6 +32,9 @@ void UABGA_Attack::ActivateAbility(const FGameplayAbilitySpecHandle Handle, cons
 	
 	//Task 실행
 	PlayAttackTask->ReadyForActivation();
+
+	//콤보 타이머
+	StartComboTimer();
 }
 
 void UABGA_Attack::CancelAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateCancelAbility)
@@ -42,11 +48,24 @@ void UABGA_Attack::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGa
 
 	AABCharacterBase* ABCharacter = CastChecked<AABCharacterBase>(ActorInfo->AvatarActor.Get());
 	ABCharacter->GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_Walking);
+
+	// 모든 몽타주 재생이 끝나면
+	CurrentComboData = nullptr;
+	CurrentCombo = 0;
+	HasNextComboInput = false;
 }
 
 void UABGA_Attack::InputPressed(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
 {
-	ABGAS_LOG(LogABGAS,Log, TEXT("Begin"));
+
+	if (!ComboTimerHandle.IsValid())
+	{
+		HasNextComboInput = false;
+	}
+	else
+	{
+		HasNextComboInput = true;
+	}
 }
 
 void UABGA_Attack::OnCompleteCallback()
@@ -63,4 +82,38 @@ void UABGA_Attack::OnInterruptedCallback()
 	bool bReplicatedEndAbility = true;
 	bool bWasCancelled = true;
 	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, bReplicatedEndAbility, bWasCancelled);
+}
+
+FName UABGA_Attack::GetNextSection()
+{
+	CurrentCombo = FMath::Clamp(CurrentCombo + 1, 1, CurrentComboData->MaxComboCount);
+	FName NextSection = *FString::Printf(TEXT("%s%d"), *CurrentComboData->MontageSectionNamePrefix, CurrentCombo);
+	return NextSection;
+}
+
+void UABGA_Attack::StartComboTimer()
+{
+	int32 ComboIndex = CurrentCombo - 1;
+	ensure(CurrentComboData->EffectiveFrameCount.IsValidIndex(ComboIndex));
+
+	// 콤보 유효시간 체크
+	float ComboEffectiveTime = CurrentComboData->EffectiveFrameCount[ComboIndex] / CurrentComboData->FrameRate;
+
+	// 0초보다 크면 유효
+	if (ComboEffectiveTime > 0.0f)
+	{
+		GetWorld()->GetTimerManager().SetTimer(ComboTimerHandle, this, &UABGA_Attack::CheckComboInput, ComboEffectiveTime, false);
+	}
+}
+
+void UABGA_Attack::CheckComboInput()
+{
+	// 함수안에들어왔으면 타이머 무효화
+	ComboTimerHandle.Invalidate();
+	if (HasNextComboInput)
+	{
+		MontageJumpToSection(GetNextSection());
+		StartComboTimer();
+		HasNextComboInput = false;
+	}
 }
---
+++ Source/ArenaBattleGAS/GA/AT/ABAT_JumpAndWaitForLanding.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GA/AT/ABAT_JumpAndWaitForLanding.h"
+#include "ABAT_JumpAndWaitForLanding.h"
+#include "GameFramework/Character.h"
+
+UABAT_JumpAndWaitForLanding::UABAT_JumpAndWaitForLanding()
+{
+
+}
+
+UABAT_JumpAndWaitForLanding* UABAT_JumpAndWaitForLanding::CreateTask(UGameplayAbility* OwningAbility)
+{
+	UABAT_JumpAndWaitForLanding* NewTask = NewAbilityTask<UABAT_JumpAndWaitForLanding>(OwningAbility);
+	return NewTask;
+}
+
+void UABAT_JumpAndWaitForLanding::Activate()
+{
+	Super::Activate();
+	ACharacter* Character = CastChecked<ACharacter>(GetAvatarActor());
+	Character->LandedDelegate.AddDynamic(this, &UABAT_JumpAndWaitForLanding::OnLandedCallback);
+	Character->Jump();
+
+	//점프가 언제 끝날지 모름 ( 높이에 따라 점프가 다르니까)
+	// SetWaitingOnAvatar 함수 호출로 AT의 상태를 Waiting 으로 바꿔줌
+	SetWaitingOnAvatar();
+
+	// ACharacter 에서 착지한것을 알려주는 delegate가 있음 -> LandedDelegate
+}
+
+void UABAT_JumpAndWaitForLanding::OnDestroy(bool AbilityEnded)
+{
+	ACharacter* Character = CastChecked<ACharacter>(GetAvatarActor());
+	Character->LandedDelegate.RemoveDynamic(this, &UABAT_JumpAndWaitForLanding::OnLandedCallback);
+	Super::OnDestroy(AbilityEnded);
+}
+
+void UABAT_JumpAndWaitForLanding::OnLandedCallback(const FHitResult& Hit)
+{
+	// 알려줄지 안알려줄지 설정 가능 -> ShouldBroadcastAbilityTaskDelegates
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		OnComplete.Broadcast();
+	}
+}
