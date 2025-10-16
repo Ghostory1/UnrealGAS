0968814c1d1272fa982de1492fa30d2411dcea39
Ghostory <ume6514@gmail.com>
2025-10-16 20:00:41 +0900

    Add AttributeSet

--- Source/ArenaBattleGAS/Attribute/ABCharacterAttributeSet.cpp
+++ Source/ArenaBattleGAS/Attribute/ABCharacterAttributeSet.cpp
@@ -2,12 +2,13 @@
 
 
 #include "Attribute/ABCharacterAttributeSet.h"
-
+#include "ArenaBattleGAS.h"
 UABCharacterAttributeSet::UABCharacterAttributeSet() :
 	AttackRange(100.0f),
 	MaxAttackRange(300.0f),
 	AttackRadius(50.0f),
 	MaxAttackRadius(150.0f),
+	AttackRate(30.0f),
 	MaxAttackRate(100.0f),
 	MaxHealth(100.0f)
 {
@@ -17,8 +18,16 @@ UABCharacterAttributeSet::UABCharacterAttributeSet() :
 
 void UABCharacterAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
 {
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.0f, GetMaxHealth());
+	}
 }
 
 void UABCharacterAttributeSet::PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue)
 {
+	if (Attribute == GetHealthAttribute())
+	{
+		ABGAS_LOG(LogABGAS, Log, TEXT("Health : %f -> %f"), OldValue, NewValue);
+	}
 }
--- Source/ArenaBattleGAS/GA/ABGA_AttackHitCheck.cpp
+++ Source/ArenaBattleGAS/GA/ABGA_AttackHitCheck.cpp
@@ -7,6 +7,8 @@
 #include "AbilitySystemBlueprintLibrary.h"
 #include "GA/AT/ABAT_Trace.h"
 #include "GA/TA/ABTA_Trace.h"
+#include "Attribute/ABCharacterAttributeSet.h"
+#include "ArenaBattleGAS.h"
 
 UABGA_AttackHitCheck::UABGA_AttackHitCheck()
 {
@@ -28,6 +30,27 @@ void UABGA_AttackHitCheck::OnTraceResultCallback(const FGameplayAbilityTargetDat
 	{
 		FHitResult HitResult = UAbilitySystemBlueprintLibrary::GetHitResultFromTargetData(TargetDataHandle, 0);
 		ABGAS_LOG(LogABGAS, Log, TEXT("Target %s Detected"),*(HitResult.GetActor()->GetName()));
+
+		UAbilitySystemComponent* SourceASC = GetAbilitySystemComponentFromActorInfo_Checked();
+		UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(HitResult.GetActor());
+		if (!SourceASC || !TargetASC)
+		{
+			ABGAS_LOG(LogABGAS, Error, TEXT("SourceASC , TargetASC Not Founded !!"));
+			return;
+		}
+		//SourceAttribute 에서 값을 읽고
+		//TargetAttribute의 값을 변경해야하니 const를 제거 해줘야함 -> const_cast
+		//하지만 좋은 방법은 아님 const_cast 보다는 다음에 게임플레이 이펙트를 통해 바꿔주는것이 좋음
+		const UABCharacterAttributeSet* SourceAttribute = SourceASC->GetSet<UABCharacterAttributeSet>();
+		UABCharacterAttributeSet* TargetAttribute = const_cast<UABCharacterAttributeSet*>(TargetASC->GetSet<UABCharacterAttributeSet>());
+		if (!SourceAttribute || !TargetAttribute)
+		{
+			ABGAS_LOG(LogABGAS, Error, TEXT("SourceAttribute , TargetAttribute Not Founded !!"));
+			return;
+		}
+
+		const float AttackDamage = SourceAttribute->GetAttackRate();
+		TargetAttribute->SetHealth(TargetAttribute->GetHealth() - AttackDamage);
 	}
 	bool bReplicatiedEndAbility = true;
 	bool bWasCancelled = false;
